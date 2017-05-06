f73f109406d1fa77f65f3107fce89ae60a163f76
lgvaz <lucasgouvaz@gmail.com>
2017-05-06 14:02:14 -0300

    Added docstring

--- main.py
+++ main.py
@@ -4,13 +4,23 @@ import recursive_backtracer
 import a_star
 import depth_first
 
-def start(num_rows=50, num_cols=50, WIDTH=601, HEIGHT=601, render=True):
+def start(search, num_rows=50, num_cols=50, WIDTH=601, HEIGHT=601, render=True):
+    ''' Generate and solve mazes.
+
+    Args:
+        search (str): Pathfind algorithm to be used. (a_star, depth_first)
+        num_rows (int): Number of cells of each column
+        num_cols (int): Number of cells of each row
+        WIDTH (int): Screen width (Number of pixels)
+        HEIGHT (int): Screen height (Number of pixels)
+        render (bool): Whether or not to render maze while creating and solving
+    '''
     # Create pygame window
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
     # Create maze
     maze = recursive_backtracer.create_maze(screen, num_rows, num_cols, render)
-    # Use a-star to find solution
-    a_star.search(maze, render)
+    # Use specified algorithm to find solution
+    eval(search).search(maze, render)
 
     while True:
         maze.draw()
